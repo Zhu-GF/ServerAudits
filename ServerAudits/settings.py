4874698addc3d65c53cf05c819633878a2782907
朱广飞 <1464287479@qq.com>
2018-01-13 09:52:57 +0800

    修改

--- ServerAudits/settings.py
+++ ServerAudits/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'server_audit',
         'USER':'root',
-        'PASSWORD':'1234',
+        'PASSWORD':'',
         'HOST':'localhost',
         'PORT':3306
     }
